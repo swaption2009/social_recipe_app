b74e4ad62fdd723147bfd1951646fca3873c6ef8
swaption2009 <swaption2009@gmail.com>
2016-09-15 13:30:30 -0700

    create 2 layouts & stylize index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :sidebar_values
+  layout :layout_by_resource
+
+
+  def sidebar_values
+    @food_preferences = FoodPreference.all
+    @food_types = FoodType.all
+    @cuisines = Cuisine.all
+  end
+
+  def layout_by_resource
+    if devise_controller?
+      "authentication"
+    else
+      "application"
+    end
+  end
+
 end
